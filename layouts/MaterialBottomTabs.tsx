d0c28f32dfbdfc3b0355d47d6db58a4bc955b47a
VivekNeel <vivek2neel@gmail.com>
2023-11-29 17:01:37 +0530

    Add material tabs

---
+++ layouts/MaterialBottomTabs.tsx
@@ -0,0 +1,18 @@
+import {
+    createMaterialBottomTabNavigator,
+    MaterialBottomTabNavigationOptions,
+  } from "@react-navigation/material-bottom-tabs";
+  
+  /* -----------------Types--------------- */
+import { EventMapBase, NavigationState } from "@react-navigation/native";
+
+  import { withLayoutContext } from "expo-router";
+  
+  const { Navigator } = createMaterialBottomTabNavigator();
+  
+  export const MaterialBottomTabs = withLayoutContext<
+    MaterialBottomTabNavigationOptions,
+    typeof Navigator,
+    NavigationState,
+    EventMapBase
+  >(Navigator);
